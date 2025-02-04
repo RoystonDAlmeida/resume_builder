cff48355f4f853fe3a3b7a5b374b81781a6e2efa
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-02-05 03:34:14 +0530

    Import of vuetify plugin

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify'; // Ensure this is correctly set up
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .mount('#app');
