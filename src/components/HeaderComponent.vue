3aae7f8cdd1325825fd5dca2c29a64de63e5db02
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-02-05 03:35:39 +0530

    Implemented Header Component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app-bar style="background: linear-gradient(to right, #9c27b0, #e91e63);">
+      <v-toolbar-title class="title">
+        <v-icon class="icon">mdi-file-document</v-icon> <!-- Use Material Design Icons -->
+            Resume Builder - Craft Your Future with Confidence
+      </v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent'
+    }
+</script>
