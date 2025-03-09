9a5d58926167759695f892c3ff465cf1f8567d16
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-03-10 02:42:42 +0530

    Added router import to main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify'; // Ensure this is correctly set up
+import router from './router';
 
-createApp(App)
+const app = createApp(App);
+
+app.provide('root', app);
+
+app
   .use(vuetify)
+  .use(router)
   .mount('#app');
