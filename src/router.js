e1ae701c863a001861741601ac305fea6e082eb3
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-03-10 02:44:44 +0530

    Created router.js file for defining routes

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import WorkExperienceComponent from './components/WorkExperienceComponent.vue';
+import ContactInfoComponent from './components/ContactInfoComponent.vue';
+
+const routes = [
+  {
+    path: '/ContactInfo',
+    name: 'contactInfo',
+    component: ContactInfoComponent,
+  },
+  {
+    path: '/WorkExperience',
+    name: 'workExperience',
+    component: WorkExperienceComponent,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
