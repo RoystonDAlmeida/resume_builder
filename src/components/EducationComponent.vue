3625d17a43dee33bc63d1edb27c12d6cc6cd2219
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-03-21 22:22:18 +0530

    Optimised EducationComponent.vue  codebase

--- src/components/EducationComponent.vue
+++ src/components/EducationComponent.vue
@@ -1,102 +1,113 @@
 <template>
-    <div class="sections">
-      <div class="education-container">
-        <div class="section-header">
-          <h2 class="section-title">{{ sectionData.title }}</h2>
-          <p class="italic-gray">*Add educational details in reverse chronological order</p>
-          <div class="arrow-container">
-            <button
-              class="nav-arrow"
-              @click="previousSection"
-              :disabled="currentSection === 0"
-            >
-              &lt;
-            </button>
-            <div class="section-number-bubble">{{ sectionData.number }}</div>
-            <button
-              class="nav-arrow"
-              @click="nextSection"
-              :disabled="currentSection >= sectionsLength - 1"
-            >
-              &gt;
-            </button>
-          </div>
+  <div class="sections">
+    <div class="education-container">
+      <div class="section-header">
+        <h2 class="section-title">{{ sectionData.title }}</h2>
+        <p class="italic-gray">*Add educational details in reverse chronological order</p>
+        <div class="arrow-container">
+          <button
+            class="nav-arrow"
+            @click="previousSection"
+            :disabled="currentSection === 0"
+          >
+            &lt;
+          </button>
+          <div class="section-number-bubble">{{ sectionData.number }}</div>
+          <button
+            class="nav-arrow"
+            @click="saveAndNext"
+            :disabled="currentSection >= sectionsLength - 1"
+          >
+            &gt;
+          </button>
         </div>
-  
-        <!-- Education Section (Scrollable Container) -->
-        <div class="scrollable-container">
-          <div v-for="(education, index) in educationSection" :key="index" class="education-section">
-            <div class="education-header">
-              <h4>Education #{{ index + 1 }}</h4>
-              <button v-if="index === 1" class="remove-button" @click="removeEducation">
-                -
-              </button>
-            </div>
-            <div class="input-row">
-              <v-text-field
-                v-model="education.name"
-                label="Degree/PUC/SSLC, Location(eg: BE in Computer Science/PUC/AISSCE(Class 12), Udipi)"
-                :error-messages="educationNameError[index]"
-                @input="validateEducationName(index)"
-              ></v-text-field>
-            </div>
-  
-            <div class="input-row">
-              <v-text-field
-                v-model="education.insituteName"
-                label="Institute / School"
-                :error-messages="insituteNameError[index]"
-                @input="validateInstituteName(index)"
-              ></v-text-field>
-            </div>
-  
-            <div class="input-row">
-              <v-text-field
-                v-model="education.universityName"
-                label="University / Board"
-                :error-messages="universityNameError[index]"
-                @input="validateUniversityName(index)"
-              ></v-text-field>
-            </div>
-  
-            <div class="input-row">
-              <v-text-field
-                v-model="education.YearOfPassing"
-                label="Date of Passing"
-                type="date"
-                :error-messages="YearOfPassingError[index]"
-                @input="validateYearOfPassing(index)"
-              ></v-text-field>
-            </div>
-  
-            <div class="input-row">
-              <v-text-field
-                v-model="education.percentage"
-                label="Percentage / GPA"
-                :error-messages="percentageError[index]"
-                @input="validatePercentage(index)"
-              ></v-text-field>
-            </div>
-          </div>
-        </div>
-  
-        <!-- Add Education Button -->
-        <button
-          v-if="educationSection.length < 4"
-          class="add-button pink-button"
-          @click="addEducation"
-        >
-          + Add Education Details
-        </button>
-  
-        <!-- Next Button -->
-        <button class="next-button" @click="saveAndNext">Next</button>
       </div>
+
+      <!-- Education Section (Scrollable Container) -->
+      <div class="scrollable-container">
+        <div
+          v-for="(education, index) in educationSection"
+          :key="index"
+          class="education-section"
+        >
+          <div class="education-header">
+            <h4>Education #{{ index + 1 }}</h4>
+            <button v-if="index > 0" class="remove-button" @click="removeEducation(index)">
+              -
+            </button>
+          </div>
+          <div class="input-row">
+            <v-text-field
+              v-model="education.name"
+              label="Degree/PUC/SSLC, Location(eg: BE in Computer Science/PUC/AISSCE(Class 12), Udipi)"
+              :error-messages="educationNameError[index]"
+              @blur="validateEducationName(index)"
+              @input="clearEducationNameError(index)"
+            ></v-text-field>
+          </div>
+
+          <div class="input-row">
+            <v-text-field
+              v-model="education.insituteName"
+              label="Institute / School"
+              :error-messages="insituteNameError[index]"
+              @blur="validateInstituteName(index)"
+              @input="clearInstituteNameError(index)"
+            ></v-text-field>
+          </div>
+
+          <div class="input-row">
+            <v-text-field
+              v-model="education.universityName"
+              label="University / Board"
+              :error-messages="universityNameError[index]"
+              @blur="validateUniversityName(index)"
+              @input="clearUniversityNameError(index)"
+            ></v-text-field>
+          </div>
+
+          <div class="input-row">
+            <v-text-field
+              v-model="education.YearOfPassing"
+              label="Date of Passing"
+              type="date"
+              :error-messages="YearOfPassingError[index]"
+              @blur="validateYearOfPassing(index)"
+              @input="clearYearOfPassingError(index)"
+            ></v-text-field>
+          </div>
+
+          <div class="input-row">
+            <v-text-field
+              v-model="education.percentage"
+              label="Percentage / GPA"
+              :error-messages="percentageError[index]"
+              @blur="validatePercentage(index)"
+              @input="clearPercentageError(index)"
+            ></v-text-field>
+          </div>
+        </div>
+      </div>
+
+      <!-- Add Education Button -->
+      <button
+        v-if="educationSection.length < 4"
+        class="add-button pink-button"
+        @click="addEducation"
+      >
+        + Add Education Details
+      </button>
+
+      <!-- Next Button -->
+      <button class="next-button" @click="saveAndNext">Next</button>
     </div>
+  </div>
 </template>
 
 <script>
-import { ref, inject, onMounted } from 'vue';
+import { ref, inject, onMounted, nextTick } from 'vue';
+import { useFieldValidation } from '../composables/useFieldValidation';
+import { useDataPersistence } from '../composables/useDataPersistence';
 
 export default {
   name: 'EducationComponent',
@@ -107,33 +118,37 @@ export default {
     const previousSection = inject('previousSection');
     const currentSection = inject('currentSection');
     const sectionsLength = inject('sectionsLength');
-    const updateTitle = inject('updateTitle'); 
-
-    onMounted(() => {
-      updateTitle('Resume Builder | Education');
-    });
+    const updateTitle = inject('updateTitle');
 
     // Define education data
     const educationSection = ref(resumeData.value.education || [
-        {
-            name: '',
-            insituteName: '',
-            universityName: '',
-            YearOfPassing: '',
-            percentage: '',
-        },
+      {
+        name: '',
+        insituteName: '',
+        universityName: '',
+        YearOfPassing: '',
+        percentage: '',
+      },
     ]);
 
     // Error messages for validation
-    const educationNameError = ref(['']);
-    const insituteNameError = ref(['']);
-    const universityNameError = ref(['']);
-    const YearOfPassingError = ref(['']);
-    const percentageError = ref(['']);
+    const educationNameError = ref(educationSection.value.map(() => ''));
+    const insituteNameError = ref(educationSection.value.map(() => ''));
+    const universityNameError = ref(educationSection.value.map(() => ''));
+    const YearOfPassingError = ref(educationSection.value.map(() => ''));
+    const percentageError = ref(educationSection.value.map(() => ''));
 
-    // Validation rules
-    // Validation rules
-    const required = (value) => !!value || 'This field is required';
+    const { validateField } = useFieldValidation();
+
+    onMounted(() => {
+      updateTitle('Resume Builder | Education');
+      // Load education data from localStorage
+      const storedResumeData =
+        JSON.parse(localStorage.getItem('resumeData')) || {};
+      if (storedResumeData.educationSection) {
+        educationSection.value = storedResumeData.education;
+      }
+    });
 
     const addEducation = () => {
       if (educationSection.value.length < 4) {
@@ -154,65 +169,77 @@ export default {
       }
     };
 
-    const removeEducation = () => {
-      if (educationSection.value.length > 1) {
-        educationSection.value.pop();
-        educationNameError.value.pop();
-        insituteNameError.value.pop();
-        universityNameError.value.pop();
-        YearOfPassingError.value.pop();
-        percentageError.value.pop();
-      }
+    const removeEducation = (index) => {
+      educationSection.value.splice(index, 1);
+      educationNameError.value.splice(index, 1);
+      insituteNameError.value.splice(index, 1);
+      universityNameError.value.splice(index, 1);
+      YearOfPassingError.value.splice(index, 1);
+      percentageError.value.splice(index, 1);
     };
 
-    const validateEducationName = (index) => {
-      if (!educationSection.value[index].name) {
-        educationNameError.value[index] = 'Degree/PUC/SSLC, Location is required';
-      } else {
-        educationNameError.value[index] = '';
-      }
-    };
+    const validateEducationName = (index) =>
+      validateField(
+        educationSection.value[index].name,
+        educationNameError.value,
+        'Degree/PUC/SSLC, Location is required',
+        null,
+        null,
+        index
+      );
 
-    const validateInstituteName = (index) => {
-      if (!educationSection.value[index].insituteName) {
-        insituteNameError.value[index] = 'Institute / School is required';
-      } else {
-        insituteNameError.value[index] = '';
-      }
-    };
+    const validateInstituteName = (index) =>
+      validateField(
+        educationSection.value[index].insituteName,
+        insituteNameError.value,
+        'Institute / School is required',
+        null,
+        null,
+        index
+      );
 
-    const validateUniversityName = (index) => {
-      if (!educationSection.value[index].universityName) {
-        universityNameError.value[index] = 'University / Board is required';
-      } else {
-        universityNameError.value[index] = '';
-      }
-    };
+    const validateUniversityName = (index) =>
+      validateField(
+        educationSection.value[index].universityName,
+        universityNameError.value,
+        'University / Board is required',
+        null,
+        null,
+        index
+      );
 
-    const validateYearOfPassing = (index) => {
-      if (!educationSection.value[index].YearOfPassing) {
-        YearOfPassingError.value[index] = 'Year of Passing is required';
-      } else {
-        YearOfPassingError.value[index] = '';
-      }
-    };
+    const validateYearOfPassing = (index) =>
+      validateField(
+        educationSection.value[index].YearOfPassing,
+        YearOfPassingError.value,
+        'Year of Passing is required',
+        null,
+        null,
+        index
+      );
 
     const validatePercentage = (index) => {
-        const percentage = educationSection.value[index].percentage;
-        
-        if (!percentage) {
-            percentageError.value[index] = 'Percentage is required';
+      const percentage = educationSection.value[index].percentage;
+
+      if (!percentage) {
+        percentageError.value[index] = 'Percentage is required';
+        return false;
+      } else {
+        const numericValue = parseFloat(percentage);
+
+        if (isNaN(numericValue)) {
+          percentageError.value[index] =
+            'Invalid input. Please enter a valid number.';
+          return false;
+        } else if (numericValue < 0.0 || numericValue > 100.0) {
+          percentageError.value[index] =
+            'Percentage / CGPA must be between 0.0 and 100.0';
+          return false;
         } else {
-            const numericValue = parseFloat(percentage);
-            
-            if (isNaN(numericValue)) {
-            percentageError.value[index] = 'Invalid input. Please enter a valid number.';
-            } else if (numericValue < 0.0 || numericValue > 100.0) {
-            percentageError.value[index] = 'Percentage / CGPA must be between 0.0 and 100.0';
-            } else {
-            percentageError.value[index] = '';
-            }
+          percentageError.value[index] = '';
+          return true;
         }
+      }
     };
 
     const validateFields = () => {
@@ -226,15 +253,11 @@ export default {
       percentageError.value = educationSection.value.map(() => '');
 
       educationSection.value.forEach((education, index) => {
-        validateEducationName(index);
-        validateInstituteName(index);
-        validateUniversityName(index);
-        validateYearOfPassing(index);
-        validatePercentage(index);
-
-        if (educationNameError.value[index] || insituteNameError.value[index] || universityNameError.value[index] || YearOfPassingError.value[index] || percentageError.value[index]) {
-          isValid = false;
-        }
+        if (!validateEducationName(index)) isValid = false;
+        if (!validateInstituteName(index)) isValid = false;
+        if (!validateUniversityName(index)) isValid = false;
+        if (!validateYearOfPassing(index)) isValid = false;
+        if (!validatePercentage(index)) isValid = false;
       });
 
       return isValid;
@@ -245,13 +268,32 @@ export default {
         // Save education data to resumeData in local storage
         let resumeData = JSON.parse(localStorage.getItem('resumeData')) || {};
 
-        resumeData.education = educationSection.value;
+        resumeData.educationSection = educationSection.value;
 
         localStorage.setItem('resumeData', JSON.stringify(resumeData));
 
         nextSection();
       }
     };
+
+    // Methods to clear error messages
+    const clearEducationNameError = (index) => {
+      educationNameError.value[index] = '';
+    };
+    const clearInstituteNameError = (index) => {
+      insituteNameError.value[index] = '';
+    };
+    const clearUniversityNameError = (index) => {
+      universityNameError.value[index] = '';
+    };
+    const clearYearOfPassingError = (index) => {
+      YearOfPassingError.value[index] = '';
+    };
+    const clearPercentageError = (index) => {
+      percentageError.value[index] = '';
+    };
+
+    useDataPersistence(resumeData, { educationSection });
 
     return {
       resumeData,
@@ -274,7 +316,11 @@ export default {
       validateUniversityName,
       validateYearOfPassing,
       validatePercentage,
-      required,
+      clearEducationNameError,
+      clearInstituteNameError,
+      clearUniversityNameError,
+      clearYearOfPassingError,
+      clearPercentageError,
     };
   },
 };
