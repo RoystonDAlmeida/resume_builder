03290581a284b3b895f26d2a7609cca06f404bb4
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-03-21 22:18:24 +0530

    Implemented App.vue with helper functions

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import {  provide, computed, defineAsyncComponent } from 'vue';
+import { provide, computed, defineAsyncComponent } from 'vue';
 import './styles/styles.css';
 import HeaderComponent from './components/HeaderComponent.vue';
 import { useLocalStorage } from './composables/useLocalStorage';
@@ -21,31 +21,35 @@ const componentNames = [
   'EducationComponent',
   'TechnicalSkillsComponent',
   'SoftSkillsComponent',
-  'LanguagesComponent'
+  'LanguagesComponent',
 ];
 
 export default {
   components: {
     HeaderComponent,
     ...Object.fromEntries(
-      componentNames.map(name => [
+      componentNames.map((name) => [
         name,
-        defineAsyncComponent(() => import(`./components/${name}.vue`))
+        defineAsyncComponent(() => import(`./components/${name}.vue`)),
       ])
-    )
+    ),
   },
 
   setup() {
     const { updateTitle } = useTitle();
     provide('updateTitle', updateTitle);
 
-    const { resumeData } = useLocalStorage('resumeData', {
-      objective: '',
-      fullName: '',
-      phoneNumber: '',
-      email: '',
-      linkedin: '',
-    }, 12 * 60 * 60 * 1000); // 12 hours expiration
+    const { resumeData } = useLocalStorage(
+      'resumeData',
+      {
+        objective: '',
+        fullName: '',
+        phoneNumber: '',
+        email: '',
+        linkedin: '',
+      },
+      12 * 60 * 60 * 1000
+    ); // 12 hours expiration
 
     const {
       sections,
