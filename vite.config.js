96b00328c0b6b869be85c68d9b5fada99b591519
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-03-22 21:58:39 +0530

    Added docker server configuration in vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
   ],
+  
+  // Docker server config
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+
   resolve: {
     alias: {
       '@': '/src',
